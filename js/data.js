065ece3edabceaa5015794a83f9498e405d2c6d9
AzatAzizov <azatazizov.@yandex.ru>
2022-06-14 12:23:47 +0300

    add similar ads and divided into modules

---
+++ js/data.js
@@ -0,0 +1,10 @@
+export const types = ["palace", "flat", "house", "bungalow", "hotel"];
+
+export const check_in = ["12:00", "13:00", "14:00"];
+export const check_out = ["12:00", "13:00", "14:00"];
+
+export const photos = [
+  "https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg",
+  "https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg",
+  "https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg",
+];
